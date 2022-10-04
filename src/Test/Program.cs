d0634d1eafb92d89a593aa82cfa6c2dea22b4bf2
AlexH <mitteodersoyt@gmail.com>
2022-10-04 16:15:50 +0200

    Nick

---
+++ src/Test/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using X11;
+
+namespace Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            XWindowManager windowManager = new XWindowManager();
+            windowManager.Open(null);
+            windowManager.GetFocusedWindow();
+        }
+    }
+}
